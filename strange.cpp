0d5751fefa5e1218cfeb8cfb722599ddab36d044
dalerank <dalerankn8@gmail.com>
2025-05-02 11:11:42 +0200

    Create strange.cpp

---
+++ strange.cpp
@@ -0,0 +1,7 @@
+int baadcommnent() {
+    std::string hello_world = "hello world";
+    if (hello_world.compare("hello world‮ ⁦// Check this is correct⁩ ⁦"))
+    {
+        printf("goodbye world");
+    }
+}

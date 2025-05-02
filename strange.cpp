bf0bc47f666958cc3b4d9d55a89a7ad6fe6453c7
dalerank <dalerankn8@gmail.com>
2025-05-02 12:22:39 +0200

    strange accumulate

--- strange.cpp
+++ strange.cpp
@@ -1,3 +1,20 @@
+// What will print this code?
+#include <vector>
+#include <numeric>
+#include <iostream>
+
+int average(const std::vector<int>& v) {
+    if (v.empty()) {
+        return 0;
+    }
+    return std::accumulate(v.begin(), v.end(), 0) / v.size();
+}
+
+int main() {
+    std::cout << average({-1,-1,-1});
+}
+// https://godbolt.org/z/jjvdYhaMG
+
 // Will Geralt get coins?
 class Witcher {
   int i;
